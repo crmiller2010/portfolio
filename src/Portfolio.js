b8046a50e26ecbd2e24dae353a3f5bc538b6a555
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-04 18:12:53 -0500

    Update Portfolio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,16 +4,16 @@ const Portfolio = () =>{
  return(
     <div>
       <div id= "port" class="portfolilo" >
-         <a href ="https://crmiller2010.github.io/Trivia-Game/"><img class="pimage" src="../public/NMT.png" alt="No Mercy Trivia"></img></a>
-         <a href ="https://crmiller2010.github.io/Module-3-challenge/"> <img class="pimage" src="../public/PW-Gen.png" alt="Password Generator"></img></a>
+         <a href ="https://crmiller2010.github.io/Trivia-Game/"><img class="pimage" src="https://github.com/crmiller2010/portfolio/blob/main/public/NMT.png?raw=true" alt="No Mercy Trivia"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-3-challenge/"> <img class="pimage" src="https://github.com/crmiller2010/portfolio/blob/main/public/PW-Gen.png?raw=true" alt="Password Generator"></img></a>
       </div>
       <div class="portfolilo">
-         <a href ="https://crmiller2010.github.io/module-5-challenge/"><img class="pimage" src="../public/WDS.png" alt="Work Day Scheduler"></img></a>
-         <a href ="https://crmiller2010.github.io/weather-API/"> <img class="pimage" src="../public/WD.png" alt="Weather Dashboard"></img></a>
+         <a href ="https://crmiller2010.github.io/module-5-challenge/"><img class="pimage" src=https://github.com/crmiller2010/portfolio/blob/main/public/WDS.png?raw=true" alt="Work Day Scheduler"></img></a>
+         <a href ="https://crmiller2010.github.io/weather-API/"> <img class="pimage" src="https://github.com/crmiller2010/portfolio/blob/main/public/WD.png?raw=true" alt="Weather Dashboard"></img></a>
       </div>
       <div class="portfolilo">
-         <a href ="https://crmiller2010.github.io/Module-4-challenge/"><img class="pimage" src="../public/Jsq.png" alt="Javascript Quiz"></img></a>
-         <a href ="https://crmiller2010.github.io/Module-2-challenge/#aboutMe"> <img class="pimage" src="../public/MFP.png" alt="My First Portfolio"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-4-challenge/"><img class="pimage" src="https://github.com/crmiller2010/portfolio/blob/main/public/Jsq.png?raw=true" alt="Javascript Quiz"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-2-challenge/#aboutMe"> <img class="pimage" src="https://github.com/crmiller2010/portfolio/blob/main/public/MFP.png?raw=true" alt="My First Portfolio"></img></a>
       </div>
 
     </div>
