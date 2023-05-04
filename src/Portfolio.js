2de52f067ea42b8fa8f4301a7247d21a9ab63228
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-04 17:28:31 -0500

    Update Portfolio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,16 +4,16 @@ const Portfolio = () =>{
  return(
     <div>
       <div id= "port" class="portfolilo" >
-         <a href ="https://crmiller2010.github.io/Trivia-Game/"><img class="pimage" src="NMT.png" alt="No Mercy Trivia"></img></a>
-         <a href ="https://crmiller2010.github.io/Module-3-challenge/"> <img class="pimage" src="PW-Gen.png" alt="Password Generator"></img></a>
+         <a href ="https://crmiller2010.github.io/Trivia-Game/"><img class="pimage" src="/NMT.png" alt="No Mercy Trivia"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-3-challenge/"> <img class="pimage" src="/PW-Gen.png" alt="Password Generator"></img></a>
       </div>
       <div class="portfolilo">
-         <a href ="https://crmiller2010.github.io/module-5-challenge/"><img class="pimage" src="WDS.png" alt="Work Day Scheduler"></img></a>
-         <a href ="https://crmiller2010.github.io/weather-API/"> <img class="pimage" src="WD.png" alt="Weather Dashboard"></img></a>
+         <a href ="https://crmiller2010.github.io/module-5-challenge/"><img class="pimage" src="/WDS.png" alt="Work Day Scheduler"></img></a>
+         <a href ="https://crmiller2010.github.io/weather-API/"> <img class="pimage" src="/WD.png" alt="Weather Dashboard"></img></a>
       </div>
       <div class="portfolilo">
-         <a href ="https://crmiller2010.github.io/Module-4-challenge/"><img class="pimage" src="jsq.png" alt="Javascript Quiz"></img></a>
-         <a href ="https://crmiller2010.github.io/Module-2-challenge/#aboutMe"> <img class="pimage" src="MFP.png" alt="My First Portfolio"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-4-challenge/"><img class="pimage" src="/jsq.png" alt="Javascript Quiz"></img></a>
+         <a href ="https://crmiller2010.github.io/Module-2-challenge/#aboutMe"> <img class="pimage" src="/MFP.png" alt="My First Portfolio"></img></a>
       </div>
 
     </div>
